9f4c1cf5b31ae93e42cafbbf431382931b7dee3b
jang <david9696@naver.com>
2025-03-15 12:05:15 +0900

    project init

---
+++ src/main/java/com/driply/backend/DriplyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.driply.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriplyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriplyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/driply/backend/DriplyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driply.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriplyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
