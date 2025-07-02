501178db1d7c3dc850afd2736be0bc919ff97e76
jang138 <david9696@naver.com>
2025-07-02 19:50:07 +0900

    feat: AuthController

---
+++ src/main/java/com/driply/backend/domains/member/customer/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.driply.backend.domains.member.customer.controller;
+
+import com.driply.backend.global.util.JWTUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final JWTUtil jwtUtil;
+
+
+}
